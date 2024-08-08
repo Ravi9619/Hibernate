c943df73cf2926ec58a9486c4942ccfda273e0c3
dubey <dubey@Ravi_Asus>
2024-08-08 11:46:37 +0530

    Hibernate PersistApp

---
+++ HB02-PersistApp/src/com/demo/test/PersistApp.java
@@ -0,0 +1,55 @@
+package com.demo.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.demo.model.Student;
+import com.demo.util.HibernateUtil;
+
+public class PersistApp {
+
+	public static void main(String[] args) {
+
+		Session session = null;
+		Transaction transaction = null;
+		boolean flag = false;
+
+		try {
+
+			session = HibernateUtil.getSession();
+
+			if (session != null) {
+				transaction = session.beginTransaction();
+			}
+
+			if (transaction != null) {
+
+				Student student = new Student();
+				student.setSid(18);
+				student.setSname("kohli");
+				student.setSaddress("RCB");
+
+				session.persist(student);
+				flag = true;
+			}
+
+		} catch (HibernateException he) {
+			he.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("object saved to database...");
+			} else {
+				transaction.rollback();
+				System.out.println("Object not saved to database...");
+			}
+			
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+	}
+
+}
