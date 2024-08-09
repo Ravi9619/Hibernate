ce013c8bcff611863749d90510b81cc6238d4bb6
dubey <dubey@Ravi_Asus>
2024-08-09 18:09:32 +0530

    Hibernate Merge Operation App

---
+++ HB19-MegeOperationApp/src/com/demo/test/LoadAndMergeApp.java
@@ -0,0 +1,56 @@
+package com.demo.test;
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.demo.model.Student;
+import com.demo.util.HibernateUtil;
+
+public class LoadAndMergeApp {
+
+	public static void main(String[] args) throws Exception {
+
+		Session session = null;
+		Transaction transaction = null;
+		boolean flag = false;
+		Student std = null;
+
+		try {
+			Student student = new Student();
+			student.setSid(5);
+			student.setSname("SKY");
+			student.setSaddress("MI");
+			student.setSage(31);
+
+			session = HibernateUtil.getSession();
+
+			if (session != null)
+				transaction = session.beginTransaction();
+
+			if (transaction != null) {
+				std = (Student) session.merge(student);
+				flag = true;
+			}
+
+		} catch (HibernateException e) {
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("Object inserted to  the database...");
+			} else {
+				transaction.rollback();
+				System.out.println("Object not inserted to the database...");
+			}
+
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+	}
+
+}
---
+++ HB19-MegeOperationApp/src/com/demo/test/MergeRecordApp.java
@@ -0,0 +1,60 @@
+package com.demo.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.demo.model.Student;
+import com.demo.util.HibernateUtil;
+
+public class MergeRecordApp {
+
+	public static void main(String[] args) throws Exception {
+
+		Session session = null;
+		Transaction transaction = null;
+		boolean flag = false;
+		Student std1 = null;
+		Student std2 = null;
+		Student std3 = null;
+
+		try {
+
+			session = HibernateUtil.getSession();
+			std1 = session.get(Student.class, 3);
+			if (session != null)
+				transaction = session.beginTransaction();
+
+			if (transaction != null) {
+				std2 = new Student();
+				std2.setSid(5);
+				std2.setSaddress("MI");
+				std2.setSname("Suryakumaryadav");
+				std2.setSage(32);
+
+				std3 = (Student) session.merge(std2);
+				System.out.println("After merging in L1Cache :: " + std3);
+				System.out.println(std1.hashCode() + ":: " + std2.hashCode() + ":: " + std3.hashCode());
+				flag = true;
+			}
+
+		} catch (HibernateException e) {
+			e.printStackTrace();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("Object inserted to  the database...");
+			} else {
+				transaction.rollback();
+				System.out.println("Object not inserted to the database...");
+			}
+
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+	}
+
+}
